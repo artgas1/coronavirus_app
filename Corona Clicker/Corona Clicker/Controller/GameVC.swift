e4c086503b502827489d5cedae61ab3441a61ab9
Egor Fedyaev <azurit78@gmail.com>
2020-03-18 21:42:01 +0300

    ADDED: taps per second, contaigousness coeffitient, changed animation

--- Corona Clicker/Corona Clicker/Controller/GameVC.swift
+++ Corona Clicker/Corona Clicker/Controller/GameVC.swift
@@ -21,8 +21,14 @@ class GameVC: UIViewController {
     
     var gameStarted = false
     var counter = 0
+    var runCounter: CGFloat = 0.0
+    var tapsPerSec: CGFloat = 0.0
+    var coeffitient = 1
     
     override func viewDidLoad() {
+        multiplyer.text = "X1"
+        tapCounter.text = "0 DECEASED"
+        tapSpeed.text = "0.0 people per second"
         observeTaps()
         super.viewDidLoad()
     }
@@ -37,13 +43,13 @@ class GameVC: UIViewController {
         let duration: CGFloat = 0.2
         let delay: CGFloat = 0.2
         
-        gameTitle.fadeOut(duration: duration, delay: 0.0) {
+        gameTitle.fadeOut(duration: duration, delay: delay) {
             self.gameTitle.removeFromSuperview()
         }
-        gameSubtitle.fadeOut(duration: duration, delay: 0.0) {
+        gameSubtitle.fadeOut(duration: duration, delay: delay) {
             self.gameSubtitle.removeFromSuperview()
         }
-        callToAction.fadeOut(duration: duration, delay: 0.0) {
+        callToAction.fadeOut(duration: duration, delay: delay) {
             self.callToAction.removeFromSuperview()
         }
         
@@ -53,17 +59,45 @@ class GameVC: UIViewController {
         multiplyer.fadeIn(duration: duration, delay: delay)
     }
     
+    func startTimer() {
+        let timer = Timer(timeInterval: 0.2, target: self, selector: #selector(self.fireTimer), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
+    }
+    
+    @objc func fireTimer() {
+        runCounter += 0.2
+        tapsPerSec = CGFloat(counter) / CGFloat(runCounter)
+        tapSpeed.text = "\(tapsPerSec) people per second"
+        tapSpeed.text = String(format: "%.2f people per second", tapsPerSec)
+        
+        if runCounter > 5.0 && tapsPerSec > 5.0 {
+            coeffitient = 2
+            multiplyer.text = "X\(coeffitient)"
+        }
+        
+        if runCounter > 10.0 && tapsPerSec > 10.0 {
+            coeffitient = 3
+            multiplyer.text = "X\(coeffitient)"
+        }
+        
+        if runCounter > 30.0 && tapsPerSec > 20.0 {
+            coeffitient = 3
+            multiplyer.text = "X\(coeffitient)"
+        }
+    }
+    
     @objc func onVirusTap() {
-        let duration: CGFloat = 0.3
+        let duration: CGFloat = 0.5
         let delay: CGFloat = 0.0
-        let scaleX: CGFloat = 1.1
-        let scaleY: CGFloat = 1.1
+        let scaleX: CGFloat = 1.08
+        let scaleY: CGFloat = 1.08
         
         if !gameStarted {
             gameStarted = true
+            startTimer()
             changeAppearence()
         }
-        counter += 1
+        counter += 1 * coeffitient
         tapCounter.text = "\(counter) DECEASED"
         
         virus.scaleOutIn(duration: duration, delay: delay, scaleX: scaleX, scaleY: scaleY)
