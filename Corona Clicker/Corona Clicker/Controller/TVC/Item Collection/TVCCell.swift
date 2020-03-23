6176a77f64485143f7cffe9390357795645de5b4
Egor Fedyaev <azurit78@gmail.com>
2020-03-23 22:22:29 +0300

    ADDED: Saves selected items

--- Corona Clicker/Corona Clicker/Controller/TVC/Item Collection/TVCCell.swift
+++ Corona Clicker/Corona Clicker/Controller/TVC/Item Collection/TVCCell.swift
@@ -27,10 +27,20 @@ class TVCCell: UITableViewCell {
 
     public func configure(items: [Item]) {
         self.items = items
+        collectionView.reloadData()
+        collectionView.layoutIfNeeded()
+        markSelectedItem()
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func markSelectedItem() {
+        let currentCellIP = IndexPath(row: DataService.currentItemID, section: 0)
+        if let cell = collectionView.cellForItem(at: currentCellIP) as? ItemCell {
+            cell.itemTitle.textColor = .red
+        }
     }
 
 }
@@ -67,6 +77,11 @@ extension TVCCell: UICollectionViewDelegate, UICollectionViewDataSource, UIColle
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // MARK: - TODO: Add selected implementation
         if items[indexPath.item].isLocked != true {
+            let currentCellIP = IndexPath(row: DataService.currentItemID, section: 0)
+            if let cell = collectionView.cellForItem(at: currentCellIP) as? ItemCell {
+                cell.itemTitle.textColor = .white
+            }
+            
             if let cell = collectionView.cellForItem(at: indexPath) as? ItemCell {
                 cell.animateSelection()
                 DataService.currentItemID = indexPath.item
