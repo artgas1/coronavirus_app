e1969cd505480bab4e5ad350603d941ee3130cc6
DartMalak <andrejhromov@MacBook-Pro-Andrej.local>
2020-03-27 17:29:33 +0300

    Перенесены storybord и purchaseVC из ebala

--- Corona Clicker/Corona Clicker/Controller/PurchaseVC.swift
+++ Corona Clicker/Corona Clicker/Controller/PurchaseVC.swift
@@ -11,22 +11,36 @@ import StoreKit
 
 class PurchaseVC: UIViewController, SKPaymentTransactionObserver {
     
-    let item: Item! = nil
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var purchaseBtn: UIButton!
+    @IBOutlet weak var purchaseVirusImage: UIImageView!
+    public var item: Item?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
         SKPaymentQueue.default().add(self)
     }
-
+    
+    func setupUI() {
+        guard let item = self.item as? Item else { return }
+        itemName.text = item.title
+        itemName.textAlignment = .center
+        self.purchaseVirusImage?.image = item.image
+        self.purchaseBtn.titleLabel?.font = UIFont(name: "Bungee-Regular", size: 16)
+        self.purchaseBtn.cornerRadius = 5.0
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     @IBAction func purchasePressed(_ sender: Any) {
         if SKPaymentQueue.canMakePayments() {
             let paymentRequest = SKMutablePayment()
-            paymentRequest.productIdentifier = item.purchaseID!
+            paymentRequest.productIdentifier = item!.purchaseID
             SKPaymentQueue.default().add(paymentRequest)
-        } else {
-            
-        }
+        } 
     }
     
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
